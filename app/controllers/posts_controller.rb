234f687f3046cd697a8ccd63a373abb113d42a5b
Antonio de Leon <tonichi@gmail.com>
2015-08-01 23:50:39 -0700

    Generated initial rails application + edit & get routes for Post

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,56 @@
+class PostsController < ApplicationController
+
+	def index # this method is basically teling us to render this text on the page.
+		@posts = Post.all
+		render :index
+	end
+
+	def new #this method renders the new.html.erb page
+		render :new
+	end
+
+	def create
+		# new post data from form
+		post_params = params.require(:post).permit(:name, :text)
+		# create new post in db
+		post = Post.create(post_params)
+		# redirect to posts' show page
+		redirect_to "/posts/#{post.id}"
+
+	end
+
+	# GET /posts/:id
+	def show 
+		# set id from url params
+		post_id = params[:id]
+		# find post in db by its id
+		@post = Post.find(post_id)
+		# render the show view
+		render :show
+  end
+
+  # GET /planes/:id/edit
+  def edit
+    # set id from url params
+    post_id = params[:id]
+    # find plane in db by its id
+    @post = Post.find(post_id)
+    # render edit view
+    render :edit
+  end
+
+  	# PUT /posts/:id
+  def update
+  	# set id from url params
+  	post_id = params[:id]
+  	# find post in db by its id
+  	post = Post.find(post_id)
+  	# updated post data from form 
+  	post_params = params.require(:post).permit(:name, :text)
+  	# update the post in db
+  	post.update_attributes(post_params)
+  	# redirect to post's show page
+  	redirect_to "/posts/#{post.id}"
+  end
+
+end
